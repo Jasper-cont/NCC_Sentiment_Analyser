14c41aa1778bcd8e742358c77dc5e127f2911cc6
Jasper Meyer <meyerjasper@gmail.com>
2023-09-08 17:09:26 +0100

    Fixing a coule of niggles, no functionality added

--- authors_most_influential_posts.py
+++ authors_most_influential_posts.py
@@ -5,6 +5,7 @@ import nltk
 import pprint
 from nltk.sentiment import SentimentIntensityAnalyzer
 from praw.models import MoreComments
+import regex as re
 # from top_level_load import TopLvlComments
 
 def iter_top_level(comments):
@@ -19,6 +20,8 @@ def iter_top_level(comments):
 
 def get_top_posts_by_authors(reddit, authors_sorted, post_limit_per_author):
     top_posts = []
+    ticker_pattern = r'\b[$]?[A-Z0-9]{2,}\b'
+    tickers_found = []
     for subreddit in authors_sorted.keys():
         for author in authors_sorted[str(subreddit)].keys():
             try:
@@ -31,8 +34,23 @@ def get_top_posts_by_authors(reddit, authors_sorted, post_limit_per_author):
 
                 # Append each post to the list and extract the top level comments
                 for post in author_top_posts:
-                    post_comments = iter_top_level(post.comments)
-                    top_posts.append((author, post.title, post.selftext, post_comments, post.score))
+                    print("0")
+                    tickers_found = re.findall(ticker_pattern, post.selftext)
+                    if len(tickers_found) > 0:
+                        print("1")
+                        if tickers_found[0] in company_tickers:
+                            print("2")
+                            post_comments = iter_top_level(post.comments)
+                            print("3")
+                            top_posts.append((author, post.title, post.selftext, post_comments, post.score, tickers_found[0]))
+                            print("4")
+
+                    else: 
+                        print("1")
+                        post_comments = iter_top_level(post.comments)
+                        print("2")
+                        top_posts.append((author, post.title, post.selftext, post_comments, post.score, False))
+                        print("3")
 
             except Exception as e:
                 print(f"Failed to get posts for author {author}: {str(e)}")
@@ -46,19 +64,14 @@ def get_top_posts_by_authors(reddit, authors_sorted, post_limit_per_author):
 if __name__ == "__main__":
     #
     with open('candidate_redditors.json', 'r') as f:
-            historical_redditors = f.read()
+        historical_redditors = f.read()
     if len(historical_redditors) < 1:
-            historical_redditors = {}
+        historical_redditors = {}
     else:
         historical_redditors = json.loads(historical_redditors)
 
-    # authors_sorted here will be the sorted karma list of users
-    # authors_sorted sorted is a json
-
-
-
-    # Get the top 10 authors from your previous step
-    #top_authors = [author for author, karma in ]
+    with open('company_ticker.json', 'r') as f:
+        company_tickers = json.loads(f.read())
 
     # Create a Reddit instance
     reddit_s = secrets()
@@ -73,7 +86,6 @@ if __name__ == "__main__":
 
     # Use the function
     top_posts = get_top_posts_by_authors(reddit, historical_redditors, 1)
-    # print(top_posts)
 
     # Sentiment Analyse the top posts
     # nltk.download([
@@ -89,8 +101,12 @@ if __name__ == "__main__":
     # pprint.pprint(tokenised_text)
     sia = SentimentIntensityAnalyzer()
 
-    for index, (author, title, content, comments, score) in enumerate(top_posts):
+    for index, (author, title, content, comments, score, ticker) in enumerate(top_posts):
         # pprint.pprint(f"Index: {index}, Author: {author}, Post Title: {title}, Post Score: {score}, Content: {content}")
+        
+        '''
+        For better accuracy I want to setup VADER to rate individual sentences instead of full posts (or comments)
+        '''
         print(len(content))
         print(comments[0].body)
-        print(f"Title: {title}, Title Score: {sia.polarity_scores(title)}, Body Score: {sia.polarity_scores(content)}, First comment Score: {sia.polarity_scores(comments[0].body)}")
+        print(f"Title: {title}, Title Score: {sia.polarity_scores(title)}, Body Score: {sia.polarity_scores(content)}, First comment Score: {sia.polarity_scores(comments[0].body)}, Ticker: {ticker}")
