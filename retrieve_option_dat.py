d25ad6712f7645be540b1d8e0a4e928bc4de2b55
Jasper Meyer <meyerjasper@gmail.com>
2023-11-15 17:48:16 +0000

    Calculaing put call ratio for a given symbol

--- retrieve_option_dat.py
+++ retrieve_option_dat.py
@@ -2,6 +2,33 @@ import yfinance as yf
 import pandas as pd
 import numpy as np
 
+def calculate_put_call_ratio(options_data, options):
+    
+    temp_data = {
+        'expirationDate' : [],
+        'p c ratio': []
+    }
+
+    for expiry_date in options:
+        temp_data['expirationDate'].append(expiry_date)
+        puts_vol = options_data[(options_data['expirationDate'] == expiry_date) & (options_data['optionType'] == 'puts')]['volume'].sum()
+        calls_vol = options_data[(options_data['expirationDate'] == expiry_date) & (options_data['optionType'] == 'calls')]['volume'].sum()
+
+        if calls_vol > 0:
+            temp_data['p c ratio'].append( puts_vol / calls_vol )
+        else:
+            temp_data['p c ratio'].append( 0 )
+
+    p_c_r_data = pd.DataFrame(temp_data)
+    return p_c_r_data
+
+
+def calculate_open_interest(options_data):
+    temp_data = {
+        'expirationDate' : [],
+        'open interest': []
+    }
+
 # Define the ticker symbol of the stock
 ticker_symbol = 'EOSE'
 
@@ -22,13 +49,19 @@ for expiration_date in options:
     calls = opt.calls
     puts = opt.puts
 
-    calls['ExpirationDate'] = expiration_date
-    puts['ExpirationDate'] = expiration_date
+    calls['expirationDate'] = expiration_date
+    calls['optionType'] = 'calls'
+    puts['expirationDate'] = expiration_date
+    puts['optionType'] = 'puts'
 
     # print(calls.head())
     # print(puts.head())
 
-    options_data = options_data.append(calls, ignore_index=True)
-    options_data = options_data.append(puts, ignore_index=True)
+    options_data = pd.concat([options_data, calls], ignore_index=True)
+    options_data = pd.concat([options_data, puts], ignore_index=True)
 
 print(options_data.head())
+
+put_call_ratio_data = calculate_put_call_ratio(options_data, options)
+
+print(put_call_ratio_data)
