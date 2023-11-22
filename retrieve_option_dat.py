c90dd75f7bd6642e7544700e1ccb89b884ba8ce9
Jasper Meyer <meyerjasper@gmail.com>
2023-11-22 17:45:19 +0000

    amalgamating options data and extracting histori

--- retrieve_option_dat.py
+++ retrieve_option_dat.py
@@ -6,7 +6,7 @@ def calculate_put_call_ratio(options_data, options):
     
     temp_data = {
         'expirationDate' : [],
-        'p c ratio': []
+        'putCallRatio': []
     }
 
     for expiry_date in options:
@@ -15,28 +15,12 @@ def calculate_put_call_ratio(options_data, options):
         calls_vol = options_data[(options_data['expirationDate'] == expiry_date) & (options_data['optionType'] == 'calls')]['volume'].sum()
 
         if calls_vol > 0:
-            temp_data['p c ratio'].append( puts_vol / calls_vol )
+            temp_data['putCallRatio'].append( puts_vol / calls_vol )
         else:
-            temp_data['p c ratio'].append( 0 )
+            temp_data['putCallRatio'].append( 0 )
 
     p_c_r_data = pd.DataFrame(temp_data)
     return p_c_r_data
-
-
-# def calculate_open_interest(options_data, options):
-    temp_data = {
-        'expirationDate' : [],
-        'open interest': []
-    }
-
-    for expiry_date in options:
-        temp_data['expirationDate'].append(expiry_date)
-        open_interest = options_data[(options_data['expirationDate'] == expiry_date)]['openInterest'].sum()
-        temp_data['open interest'].append( open_interest )
-
-    open_interest_data = pd.DataFrame(temp_data)
-
-    return open_interest_data
 
 # Define the ticker symbol of the stock
 ticker_symbol = 'EOSE'
@@ -69,17 +53,23 @@ for expiration_date in options:
     options_data = pd.concat([options_data, calls], ignore_index=True)
     options_data = pd.concat([options_data, puts], ignore_index=True)
 
-print(options_data.head())
+# print(options_data.head())
 
 put_call_ratio_data = calculate_put_call_ratio(options_data, options)
 
-print(put_call_ratio_data)
+# print(put_call_ratio_data)
 
 # open_interest = calculate_open_interest(options_data, options)
 open_interest = options_data.groupby('expirationDate')['openInterest'].sum().reset_index()
 
-print(open_interest)
+# print(open_interest)
 
 average_iv_by_date = options_data.groupby('expirationDate')['impliedVolatility'].mean().reset_index()
 
-print(average_iv_by_date)
+# print(average_iv_by_date)
+
+merged_options_data = pd.merge(put_call_ratio_data, open_interest, on='expirationDate', how='outer')
+
+merged_options_data = pd.merge(merged_options_data, average_iv_by_date, on='expirationDate', how='outer')
+
+print(merged_options_data)
